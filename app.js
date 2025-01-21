f8e3eee2e9177c4ec9ec91c3bca42cf52e2e6283
JosephRS06 <160061272+JosephRS06@users.noreply.github.com>
2025-01-21 15:14:16 -0600

    actualiza el código del challenge

--- app.js
+++ app.js
@@ -1,37 +1,48 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let nombres = [];
-let numeroMaximo = 20;
-//Capturar el valor del campo de entrada
+//con esto agregamos el nombre al sorteo mediante id del input 
 function agregarAmigo(){
    let agregarNombres = document.getElementById('amigo').value;
    if (agregarNombres =='') {
-        alert("por favor ingrese un nombre valido")
-   } else{
+        alert("por favor ingrese un nombre valido");
+   } else if (nombres.includes(agregarNombres)){
+    alert ('Este nombre ya esta en la lista');
+   }   else{
         nombres.push(agregarNombres);
+        agregarNombreLista(agregarNombres);
    }
-
    limpiarCaja();
    console.log(nombres);
-   return;
 }
 
+//Con esto agregamos los nombres a la lista del html
+function agregarNombreLista(nombre) {
+    let listaHtml = document.getElementById('listaAmigos'); 
+    let nuevoElementoLista = document.createElement('li');
+    nuevoElementoLista.textContent = nombre;
+    listaHtml.appendChild(nuevoElementoLista);
+}
+
+//Realiza el sorteo del nombre
 function sortearAmigo(){
-    if (nombres.length <= numeroMaximo){
-        let indiceAleatorio = Math.floor(Math.random() * nombres.length);
-        const nombreSeleccionado = nombres[indiceAleatorio];
-        alert(`el nombre seleccionado es: ${nombreSeleccionado}`)
+    //si el array esta vacio arrojara un alert
+    if (nombres.length ===0){
+        alert('la lista esta vacia, por favor ingresar nombres para sortear')
     } else{
-        alert("El array está vacío. No hay nombres para seleccionar.");
+        //realiza el sorteo con el legth del array
+        let indiceAleatorio = Math.floor(Math.random() * nombres.length);
+        // con los [] se selecciona una caracteristica especifica del array
+        let nombreSeleccionado = nombres[indiceAleatorio];
+        document.getElementById('resultado').textContent = `El ganador es: ${nombreSeleccionado}`;
+        nombres = [];
+        document.getElementById('listaAmigos').innerHTML = ''; 
     } 
 }
 
-
+//Limpia el input
 function limpiarCaja (){
     document.getElementById('amigo').value = '';
 }
 
 
-// reiniciar la aplicaciones una vez hecho el sorteo 
-// mostrar los elememntos de la lista en la pagina 
-// incluir el for 
-// ver posbles mejoras en el codigo 
+
